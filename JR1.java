58fa3489ff244745090b87f309b8fa32e1ea62b0
LUIZ NAPOLEÃO <126776758+Napolinha@users.noreply.github.com>
2023-04-06 09:14:24 -0300

    Add files via upload

--- JR1.java
+++ JR1.java
@@ -2,24 +2,29 @@
 import java.util.Scanner;
 public class JR1 {
     public static void main(String[] args) {
-        int num;
-        Scanner sc = new Scanner(System.in);
-        boolean continuar = true;
-        do{
-            try{
-                    System.out.println("Digite um número: ");
-                    num = sc.nextInt();
-                    System.out.println(' ');
-                    System.out.println("O número é: "+num);
-                    sc.close();
-                    break;
-                }catch(Exception e){
-                System.out.println("Caractere invalido");
-                break;
+       Scanner sc = new Scanner(System.in);throw
+        int num = Integer.MIN_VALUE;
+        while (true) {
+            System.out.println("Digite um número:");
+            while (true) { // enquanto não digitar um número válido, continua no while
+                try {
+                    num = Integer.parseInt(sc.nextLine());
+                    System.out.println("O número digitado é: "+ num);
+                    break; // se chegou aqui é porque o número é válido, então posso sair do while
+                } catch (NumberFormatException e) {
+                    // número inválido
+                    System.out.println("Digite um número válido");
+                }
             }
-        }while(continuar);
         
+            // se for -1, sai do while mais externo
+            if (num != Integer.MIN_VALUE){
+                break;
+
+            }
+        
+           
+        }
     }
 }
-    
 
